2ac63a8bdbc09ba080a85537c659e03442c04adb
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-26 00:08:03 -0300

    feature: change main config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as fastifyCors from 'fastify-cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -11,8 +13,13 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  app.enableCors();
+  app.register(fastifyCors, {
+    origin: (origin: any, cb: (arg0: null, arg1: boolean) => any) =>
+      cb(null, true),
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
+
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
-
 bootstrap();
