df389bfc110f66a1b9dfff7a86bb4354960a5b63
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-26 00:29:08 -0300

    edit again

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,21 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import * as fastifyCors from 'fastify-cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  app.enableCors({ origin: '*' });
+
+  app.register(fastifyCors, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*',
+    preflightContinue: true,
+  });
+
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
