0dd0346d852781438a7c0ea9a401922d1f6a953b
Juancapp <juan.cappuccio@radiumrocket.com>
2024-02-27 12:25:54 -0300

    change removeOneItem service function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,6 +30,40 @@ export class UserService {
     }
   }
 
+  async editItems(
+    userId: string,
+    productId: string,
+    quantity: number,
+  ): Promise<User> {
+    if (quantity === 1) return this.addOneItem(userId, productId);
+    if (quantity === 0) return this.removeAllItems(userId, productId);
+
+    return this.userModel
+      .findOne({ _id: userId, 'products.product': productId })
+      .exec()
+      .then((user) => {
+        if (user) {
+          return this.userModel
+            .findOneAndUpdate(
+              { _id: userId, 'products.product': productId },
+              { $set: { 'products.$.quantity': quantity } },
+              { new: true },
+            )
+            .exec();
+        } else {
+          return this.userModel
+            .findOneAndUpdate(
+              { _id: userId },
+              {
+                $push: { products: { product: productId, quantity: quantity } },
+              },
+              { new: true },
+            )
+            .exec();
+        }
+      });
+  }
+
   async addOneItem(userId: string, productId: string): Promise<User> {
     return this.userModel
       .findOneAndUpdate(
@@ -50,62 +84,29 @@ export class UserService {
       });
   }
 
-  async editItems(
-    userId: string,
-    productId: string,
-    quantity: number,
-  ): Promise<User> {
-    if (quantity === 1) return this.addOneItem(userId, productId);
-    if (quantity === 0) return this.removeAllItems(userId, productId);
-
-    return this.userModel
-      .findOne({ _id: userId, 'products.product': productId })
-      .exec()
-      .then((user) => {
-        if (user) {
-          return this.userModel
-            .findOneAndUpdate(
-              { _id: userId, 'products.product': productId },
-              { $inc: { 'products.$.quantity': quantity } },
-              { new: true },
-            )
-            .exec();
-        } else {
-          return this.userModel
-            .findOneAndUpdate(
-              { _id: userId },
-              {
-                $push: { products: { product: productId, quantity: quantity } },
-              },
-              { new: true },
-            )
-            .exec();
-        }
-      });
-  }
-
   async removeOneItem(userId: string, productId: string): Promise<User> {
-    return this.userModel
+    const user = await this.userModel
       .findOneAndUpdate(
-        {
-          _id: userId,
-          'products.product': productId,
-          'products.quantity': { $gt: 1 },
-        },
+        { _id: userId, 'products.product': productId },
         { $inc: { 'products.$.quantity': -1 } },
         { new: true },
       )
-      .exec()
-      .then((user) => {
-        if (user) return user;
-        return this.userModel
-          .findOneAndUpdate(
-            { _id: userId },
-            { $pull: { products: { product: productId } } },
-            { new: true },
-          )
-          .exec();
-      });
+      .exec();
+
+    if (
+      user.products.find((p) => p.product.toString() === productId).quantity ===
+      0
+    ) {
+      await this.userModel
+        .findOneAndUpdate(
+          { _id: userId },
+          { $pull: { products: { product: Object(productId) } } },
+          { new: true },
+        )
+        .exec();
+    }
+
+    return user;
   }
 
   async removeAllItems(userId: string, productId: string): Promise<User> {
