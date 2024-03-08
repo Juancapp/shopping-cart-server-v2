1830910ad4becf4a809f0d1c22c40343e5dcdb09
Juancapp <juan.cappuccio@radiumrocket.com>
2024-03-08 00:11:03 -0300

    rollback

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,13 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import * as FastifyFormBody from 'fastify-formbody';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-    { bodyParser: false },
   );
   app.enableCors();
-  app.register(FastifyFormBody as any);
-  await app.init();
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
